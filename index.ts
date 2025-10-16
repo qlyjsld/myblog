7be4005684aba089c6142550f5cfae8433601541
jay <wuhoipok@gmail.com>
2025-10-16 15:25:05 +0800

    post json

--- index.ts
+++ index.ts
@@ -2,23 +2,24 @@ import express from 'express'
 import cors from 'cors'
 import { fileURLToPath } from 'url'
 import { dirname, join } from 'path'
-import pgPromise from 'pg-promise';
+import pgPromise from 'pg-promise'
 
 const app = express()
 const port = 3000
 
-const pgp = pgPromise();
-const db = pgp('postgres://postgres:passwd@localhost:5432/myblog');
+const pgp = pgPromise()
+const db = pgp('postgres://postgres:passwd@localhost:5432/myblog')
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
-app.use(cors());
-app.use(express.static(join(__dirname, 'dist')));
+app.use(cors())
+app.use(express.static(join(__dirname, 'dist')))
+app.use(express.json())
 
 app.get('/api/comments', async (req, res, next) => {
     try {
-        const comments = await db.any('SELECT * FROM COMMENTS');
+        const comments = await db.any('SELECT * FROM COMMENTS')
         res.send(comments)
     }
     catch (e) {
@@ -26,33 +27,29 @@ app.get('/api/comments', async (req, res, next) => {
     }
 })
 
-app.post('/api/comments/:userid/:comment', async (req, res, next) => {
-    const params = {
-        userid: req.params.userid,
-        comment: req.params.comment
-    }
+app.post('/api/comments', async (req, res, next) => {
+    const { userid, comment } = req.body
+    const params = { userid, comment }
     try {
         const comments = await db.any(
             'INSERT INTO COMMENTS(userid, comment)\
-            VALUES(${userid}, ${comment})', params);
-        res.send('POST /api/comments/:userid/:comment succeeded')
+            VALUES(${userid}, ${comment})', params)
+        res.send('POST /api/comments succeeded')
     }
     catch (e) {
-        res.send('POST /api/comments/:userid/:comment failed')
+        res.send('POST /api/comments failed')
     }
 })
 
 app.delete('/api/comments/:id', async (req, res, next) => {
-    const params = {
-        id: req.params.id,
-    }
+    const params = { id: req.params.id }
     try {
         const comments = await db.none(
-            'DELETE FROM COMMENTS WHERE id = ${id}', params);
-        res.send('DELETE /api/comments/:id succeeded')
+            'DELETE FROM COMMENTS WHERE id = ${id}', params)
+        res.send('DELETE /api/comments succeeded')
     }
     catch (e) {
-        res.send('DELETE /api/comments/:id failed')
+        res.send('DELETE /api/comments failed')
     }
 })
 
